ecb19fc91754bc86a0acc3029a9d3634b2bf627b
atanu <atanurkm11@gmail.com>
2025-05-19 14:28:26 -0600

    accelerationAndJerkOulierFun updated

--- Feature Extraction/Feature Functions/accelerationAndJerkOulierFun.m
+++ Feature Extraction/Feature Functions/accelerationAndJerkOulierFun.m
@@ -1,109 +1,111 @@
+function [accOutlierMoveMedian, jerkOutlierMoveMedian] = accelerationAndJerkOulierFun(id, varargin)
 % Author: Atanu Giri
-% date: 12/20/2023
-
-function [accOutlierMoveMedian,jerkOutlierMoveMedian, varargout] = accelerationAndJerkOulierFun(id, varargin)
+% Date: 12/20/2023 (Modified: 05/19/2025)
 %
 % This algorithm calculates the number of outliers by analyzing subject's
-% acceleration and jerkness by movemedian method.
+% acceleration and jerkness using a moving median method.
 %
-close all; clc;
+% Usage:
+% [acc, jerk] = accelerationAndJerkOulierFun(id)
+% [acc, jerk] = accelerationAndJerkOulierFun(id, conn, true)
 
-% id = 265302;
-
+% Handle optional inputs
 if numel(varargin) < 1
     datasource = 'live_database';
-    conn = database(datasource,'postgres','1234');
+    conn = database(datasource, 'postgres', '1234');
 else
-    conn =  varargin{1};
+    conn = varargin{1};
 end
 
+% Default: do not plot
+plotFlag = false;
+if numel(varargin) >= 2
+    plotFlag = varargin{2};
+end
 
-% write query
-query = sprintf("SELECT id, distance_until_limiting_time_stamp, norm_t, norm_x, norm_y " + ...
-    "FROM ghrelin_featuretable WHERE id = %d", id);
-subject_data = fetch(conn,query);
-
-liveTableQuery = sprintf("SELECT id, playstarttrialtone " + ...
-    "FROM live_table WHERE id = %d", id);
-liveTableData = fetch(conn, liveTableQuery);
-
-subject_data = innerjoin(liveTableData, subject_data, 'Keys', 'id');
+% Combined query from both tables
+query = sprintf( ...
+    "SELECT g.id, g.distance_until_limiting_time_stamp, norm_t, norm_x, norm_y, " + ...
+    "l.playstarttrialtone FROM ghrelin_featuretable g " + ...
+    "JOIN live_table l ON g.id = l.id " + ...
+    "WHERE g.id = %d", ...
+    id);
+subject_data = fetch(conn, query);
 
 try
-    subject_data.playstarttrialtone = str2double(subject_data.playstarttrialtone);
-    if isnan(subject_data.playstarttrialtone)
-        subject_data.playstarttrialtone = 2;
-    end
+    % Parse playstarttrialtone
     subject_data.distance_until_limiting_time_stamp = str2double(subject_data.distance_until_limiting_time_stamp);
-    % Accessing PGArray data as double
-    for column = size(subject_data,2) - 2:size(subject_data,2)
-        stringAllRows = string(subject_data.(column));
-        regAllRows = regexprep(stringAllRows,'{|}','');
-        splitAllRows = split(regAllRows,',');
-        doubleData = str2double(splitAllRows);
-        subject_data.(column){1} = doubleData;
+    playTone = str2double(subject_data.playstarttrialtone);
+    if isnan(playTone)
+        playTone = 2;
     end
 
-    X = subject_data.norm_x{1};
-    Y = subject_data.norm_y{1};
-    t = subject_data.norm_t{1};
+    % Parse norm_t, norm_x, norm_y as numeric arrays
+    for colName = ["norm_t", "norm_x", "norm_y"]
+        rawStr = string(subject_data.(colName));
+        cleanedStr = regexprep(rawStr, '[{}]', '');
+        splitStr = split(cleanedStr, ',');
+        subject_data.(colName){1} = str2double(splitStr);
+    end
 
     limitingTimeIndex = 20;
-    X = X(t >= subject_data.playstarttrialtone & t <= limitingTimeIndex);
-    Y = Y(t >= subject_data.playstarttrialtone & t <= limitingTimeIndex);
-    t = t(t >= subject_data.playstarttrialtone & t <= limitingTimeIndex);
+
+    % Create coordinate table
+    data = table(subject_data.norm_t{1}, subject_data.norm_x{1}, ...
+        subject_data.norm_y{1}, 'VariableNames', {'t', 'X', 'Y'});
+
+    % Present cost (PC) range: playTone–20 sec
+    pcFilter = data.t >= playTone & data.t <= limitingTimeIndex;
+    t = data.t(pcFilter);
+    X = data.X(pcFilter);
+    Y = data.Y(pcFilter);
 
     % Calculate velocity
-    Vx = diff(X) ./ diff(t); % velocity in the X direction
-    Vy = diff(Y) ./ diff(t); % velocity in the Y direction
-
-    % Append a zero at the beginning to make the size of V match the size of t
-    Vx = [0; Vx];
+    Vx = diff(X) ./ diff(t); % X velocity
+    Vy = diff(Y) ./ diff(t); % Y velocity
+    Vx = [0; Vx]; % Pad
     Vy = [0; Vy];
 
     % Calculate acceleration
-    Ax = diff(Vx) ./ diff(t); % acceleration in the X direction
-    Ay = diff(Vy) ./ diff(t); % acceleration in the Y direction
-
-    % Append a zero at the beginning to make the size of A match the size of t
+    Ax = diff(Vx) ./ diff(t); % X acceleration
+    Ay = diff(Vy) ./ diff(t); % Y acceleration
     Ax = [0; Ax];
     Ay = [0; Ay];
 
     % Total acceleration magnitude
     A = sqrt(Ax.^2 + Ay.^2);
 
-    % Calculate acceleration outlier
-    accOutlierTF = isoutlier(A,"movmedian",5);
-    accOutlierMoveMedian = sum(accOutlierTF)/subject_data.distance_until_limiting_time_stamp;
+    % Acceleration outliers
+    accOutlierTF = isoutlier(A, "movmedian", 5);
+    accOutlierMoveMedian = sum(accOutlierTF) / subject_data.distance_until_limiting_time_stamp;
 
-    % Calculate Jerk
+    % Calculate jerk
     Jx = diff(Ax) ./ diff(t);
     Jy = diff(Ay) ./ diff(t);
-
     Jx = [0; Jx];
     Jy = [0; Jy];
 
-    % Total acceleration magnitude
     J = sqrt(Jx.^2 + Jy.^2);
-    jerkOutlierMoveMedian = sum(isoutlier(J,"movmedian",5))/subject_data.distance_until_limiting_time_stamp;
+    jerkOutlierMoveMedian = sum(isoutlier(J, "movmedian", 5)) / subject_data.distance_until_limiting_time_stamp;
 
-    %% Plotting
-    h = figure;
-    plot(t,A,'b','LineWidth',2);
-    hold on;
-    accOutlierVal = A(accOutlierTF);
-    accOutlierTime = t(accOutlierTF);
-    scatter(accOutlierTime, accOutlierVal, 'filled','Color','r');
-    title(sprintf("id = %d", id), 'Interpreter','latex');
-    hold off;
-    xlabel('Time (s)', 'FontSize', 25, 'Interpreter', 'latex');
-    ylabel('Acceleration', 'FontSize', 25, 'Interpreter', 'latex');
-
-    varargout{1} = h;
-    varargout{2} = accOutlierTime;
+    %% Optional Plotting
+    if plotFlag
+        figure;
+        plot(t, A, 'b', 'LineWidth', 2);
+        hold on;
+        accOutlierVal = A(accOutlierTF);
+        accOutlierTime = t(accOutlierTF);
+        scatter(accOutlierTime, accOutlierVal, 'filled', 'MarkerFaceColor', 'r');
+        title(sprintf("ID = %d", id), 'Interpreter', 'latex');
+        xlabel('Time (s)', 'FontSize', 25, 'Interpreter', 'latex');
+        ylabel('Acceleration', 'FontSize', 25, 'Interpreter', 'latex');
+        hold off;
+    end
 
 catch
-    sprintf("An error occured for id = %d\n", id);
+    fprintf("An error occurred for id = %d\n", id);
+    accOutlierMoveMedian = NaN;
+    jerkOutlierMoveMedian = NaN;
 end
 
 end
