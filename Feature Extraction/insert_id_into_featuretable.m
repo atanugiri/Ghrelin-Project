2225b71fe58a685ec6eee1878dd739c0bb2421e1
atanu <atanurkm11@gmail.com>
2025-05-22 16:37:50 -0600

    Removed redundancies

--- Feature Extraction/insert_id_into_featuretable.m
+++ Feature Extraction/insert_id_into_featuretable.m
@@ -5,32 +5,19 @@
 % This script inject unique ids in the ghrelin_featuretable
 %
 
-datasource = 'live_database';
-conn = database(datasource,'postgres','1234');
-dateQuery = "SELECT id, referencetime FROM live_table ORDER BY id";
-allDates = fetch(conn, dateQuery);
-allDates.referencetime = datetime(allDates.referencetime, 'Format', 'MM/dd/yyyy');
-allDates = sortrows(allDates, 'referencetime');
-startDate = datetime('11/05/2021', 'InputFormat', 'MM/dd/yyyy');
-endDate = datetime('06/24/2022', 'InputFormat', 'MM/dd/yyyy');
-endDate = endDate + days(1);
+conn = database('live_database','postgres','1234');
 
-dataInRange = allDates(allDates.referencetime >= startDate & allDates.referencetime <= endDate, :);
-idList = dataInRange.id;
-
-tableName = 'ghrelin_featuretable';
-
-% Iterate over 'id' values
 for i = 1:length(idList)
     try
         id = idList(i);
-        query = sprintf("INSERT INTO %s (id) VALUES (%d)", tableName, id);
-        exec(conn,query);
-
+        query = sprintf( ...
+            "INSERT INTO ghrelin_featuretable (id) VALUES (%d) ON CONFLICT (id) DO NOTHING", ...
+            id);
+        exec(conn, query);
+        fprintf("Inserted (or skipped existing) ID: %d\n", id);
     catch exception
-       fprintf("Error in insert_id for id = %d\n", id);
-       disp(exception.message);
+        fprintf("Error inserting ID %d: %s\n", id, exception.message);
     end
 end
 
-close(conn);  % Close the database connection
+close(conn);
