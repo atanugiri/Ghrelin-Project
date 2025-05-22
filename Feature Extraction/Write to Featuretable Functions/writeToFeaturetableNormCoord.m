8017bda5267dfb36aed4c0100a6e0894eac3302c
atanu <atanurkm11@gmail.com>
2025-05-22 16:38:12 -0600

    Removed redundancies

--- Feature Extraction/Write to Featuretable Functions/writeToFeaturetableNormCoord.m
+++ Feature Extraction/Write to Featuretable Functions/writeToFeaturetableNormCoord.m
@@ -1,38 +1,27 @@
 % Author: Atanu Giri
 % Date: 12/11/2023
 
-% clear; clc;
-datasource = 'live_database';
-conn = database(datasource,'postgres','1234');
-% dateQuery = "SELECT id, referencetime FROM live_table ORDER BY id";
-% allDates = fetch(conn, dateQuery);
-% allDates.referencetime = datetime(allDates.referencetime, 'Format', 'MM/dd/yyyy');
-% startDate = datetime('09/12/2023', 'InputFormat', 'MM/dd/yyyy');
-% endDate = datetime('12/11/2023', 'InputFormat', 'MM/dd/yyyy');
-% endDate = endDate + days(1);
-% 
-% dataInRange = allDates(allDates.referencetime >= startDate & allDates.referencetime <= endDate, :);
-% idList = dataInRange.id;
-
-loadFile = load("emptyIDs.mat");
-idList = loadFile.emptyIDs;
-
-tableName = 'ghrelin_featuretable';
-
 parfor index = 1:length(idList)
     id = idList(index);
     try
-        [normT, normX, normY] = extractNormalizedCoordinate(id, conn);
+        % Each worker must create its own connection
+        connLocal = database('live_database','postgres','1234');
+
+        [normT, normX, normY] = extractNormalizedCoordinate(id, connLocal);
+
+        normT = normT(:)'; normX = normX(:)'; normY = normY(:)';  % Ensure row vectors
 
         norm_t_string = sprintf('ARRAY[%s]', strjoin(cellstr(num2str(normT, '%.6f')), ','));
         norm_x_string = sprintf('ARRAY[%s]', strjoin(cellstr(num2str(normX, '%.6f')), ','));
         norm_y_string = sprintf('ARRAY[%s]', strjoin(cellstr(num2str(normY, '%.6f')), ','));
 
-
-        updateQuery = sprintf("UPDATE %s SET norm_t=%s, norm_x=%s, norm_y=%s " + ...
-            "WHERE id=%d", tableName, norm_t_string, norm_x_string, norm_y_string, id);
+        updateQuery = sprintf("UPDATE ghrelin_featuretable SET norm_t=%s, norm_x=%s, norm_y=%s " + ...
+            "WHERE id=%d", norm_t_string, norm_x_string, norm_y_string, id);
 
         exec(conn, updateQuery);
+        close(connLocal);
+
+        fprintf("Updated ID %d with %d points\n", id, numel(normT));
 
     catch ME
         fprintf("Calculation error in %d: %s\n", id, ME.message);
