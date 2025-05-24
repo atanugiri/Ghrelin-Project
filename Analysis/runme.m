9183501168c31c06a46efda5dc4bbd0e8cdcf528
atanu <atanurkm11@gmail.com>
2025-05-24 12:28:31 -0600

    run_me updated

--- Analysis/runme.m
+++ Analysis/runme.m
@@ -5,12 +5,12 @@ conn = database('live_database','postgres','1234');
 sal_id = treatmentIDfun('P2L1 Saline', conn);
 sal_id_str = strjoin(string(sal_id), ',');
 sal_id_q = sprintf( ...
-"SELECT g.id, g.distance_until_limiting_time_stamp, l.mazenumber " + ...
-"FROM ghrelin_featuretable g " + ...
-"INNER JOIN live_table l ON g.id = l.id " + ...
-"WHERE g.id IN (%s) " + ...
-"ORDER BY g.id", ...
-sal_id_str);
+    "SELECT g.id, g.distance_until_limiting_time_stamp, l.mazenumber " + ...
+    "FROM ghrelin_featuretable g " + ...
+    "INNER JOIN live_table l ON g.id = l.id " + ...
+    "WHERE g.id IN (%s) " + ...
+    "ORDER BY g.id", ...
+    sal_id_str);
 sal_Data = fetch(conn, sal_id_q);
 sal_Data.distance_until_limiting_time_stamp = str2double(sal_Data.distance_until_limiting_time_stamp);
 sal_Data.mazenumber = string(sal_Data.mazenumber);
@@ -22,12 +22,12 @@ sal_data_maze1 = sal_data_maze1(sal_data_maze1.distance_until_limiting_time_stam
 ghr_id = treatmentIDfun('P2L1 Ghrelin', conn);
 ghr_id_str = strjoin(string(ghr_id), ',');
 ghr_id_q = sprintf( ...
-"SELECT g.id, g.distance_until_limiting_time_stamp, l.mazenumber " + ...
-"FROM ghrelin_featuretable g " + ...
-"INNER JOIN live_table l ON g.id = l.id " + ...
-"WHERE g.id IN (%s) " + ...
-"ORDER BY g.id", ...
-ghr_id_str);
+    "SELECT g.id, g.distance_until_limiting_time_stamp, l.mazenumber " + ...
+    "FROM ghrelin_featuretable g " + ...
+    "INNER JOIN live_table l ON g.id = l.id " + ...
+    "WHERE g.id IN (%s) " + ...
+    "ORDER BY g.id", ...
+    ghr_id_str);
 ghr_Data = fetch(conn, ghr_id_q);
 ghr_Data.distance_until_limiting_time_stamp = str2double(ghr_Data.distance_until_limiting_time_stamp);
 ghr_Data.mazenumber = string(ghr_Data.mazenumber);
@@ -37,13 +37,13 @@ ghr_data_maze1 = ghr_data_maze1(ghr_data_maze1.distance_until_limiting_time_stam
 
 figure;
 for i = 1:20
-trajectoryScatterPlot(sal_data_maze1.id(i), gcf);
+    trajectoryScatterPlot(sal_data_maze1.id(i), gcf);
 end
 mazeMethods(2);
 
 figure;
 for i = 1:20
-trajectoryScatterPlot(ghr_data_maze1.id(i), gcf);
+    trajectoryScatterPlot(ghr_data_maze1.id(i), gcf);
 end
 mazeMethods(2);
 
@@ -57,7 +57,7 @@ accelerationAndJerkOulierFun(94689, conn, true); % Ghrelin
 ids = [95085, 95117, 99606, 99622, 99654, 100234];
 figure;
 for id = ids
-trajectoryPlot(id, gcf);
+    trajectoryPlot(id, gcf);
 end
 mazeMethods(2);
 
@@ -65,17 +65,62 @@ mazeMethods(2);
 ids = [96208, 96231, 96287, 96311, 96323, 96347];
 figure;
 for id = ids
-trajectoryPlot(id, gcf);
+    trajectoryPlot(id, gcf);
 end
 mazeMethods(2);
 
 %% Cumulative time in feeders
-[T1, T2] = masterPsychometricBarPlot('timein_all_conc', [], ...
-    'timein_all_conc', 'trial', [], [], {'P2L1 Saline'}, {'P2L1 Ghrelin'});
-p = ranksum(T1, T2);
+% [T1, T2] = masterPsychometricBarPlot('timein_all_conc', [], ...
+%     'timein_all_conc', 'trial', [], [], {'P2L1 Saline'}, {'P2L1 Ghrelin'});
+% p = ranksum(T1, T2);
 
 %% Curvature
-[T1, T2] = masterPsychometricBarPlot('curvature', [], 'curvature', ...
-    'trial', [], 0.75, {'P2L1 Saline'}, {'P2L1 Ghrelin'});
+% [T1, T2] = masterPsychometricBarPlot('curvature', [], 'curvature', ...
+%     'trial', [], 0.75, {'P2L1 Saline'}, {'P2L1 Ghrelin'});
+% p = ranksum(T1, T2);
 
 %% Nest example
+% figure;
+% for i = 1:20
+%     trajectoryScatterPlot(sal_data_maze1.id(i), gcf);
+% end
+% figure;
+% for i = 1:20
+%     trajectoryScatterPlot(ghr_data_maze1.id(i), gcf);
+% end
+
+
+% 05/23/2025
+% Pre-feeding (PF)
+conn = database('live_database','postgres','1234');
+P2L1_PF_id = treatmentIDfun('P2L1 Prefeeding', conn);
+P2L1L3_PF_id = treatmentIDfun('P2L1L3 Prefeeding', conn);
+PF_id = [P2L1_PF_id; P2L1L3_PF_id];
+PF_id_str = join(string(PF_id), ',');
+
+PF_data_query = sprintf("SELECT g.id, g.norm_x, g.norm_y, l.mazenumber, " + ...
+    "l.referencetime, l.xcoordinates2, l.ycoordinates2 FROM ghrelin_featuretable g JOIN " + ...
+    "live_table l ON g.id = l.id WHERE g.id IN (%s)", PF_id_str);
+PF_data = fetch(conn, PF_data_query);
+
+PF_data.mazenumber = string(PF_data.mazenumber);
+tokens = regexp(PF_data.mazenumber, '\d+', 'match');
+PF_data.mazenumber = cellfun(@(x) str2double(x{1}), tokens);
+
+%
+empty_data_flag = cellfun(@(x) isempty(x), PF_data.norm_x);
+empty_data = PF_data(empty_data_flag, :);
+
+maze_data = cell(1,4);
+
+for maze = 1:4
+    maze_filter = empty_data.mazenumber == maze;
+    maze_data{maze} = empty_data(maze_filter, :);
+end
+figure;
+for maze = 1:4
+    for i = 1:10 %height(maze_data{maze})
+        id = maze_data{maze}.id(i);
+        trajectoryScatterPlotRaw(id, gcf)
+    end
+end
