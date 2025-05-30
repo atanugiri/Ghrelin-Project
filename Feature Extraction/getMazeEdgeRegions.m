4f1a6c24aeb96bb633878ebf4893f5f356bbace5
atanu <atanurkm11@gmail.com>
2025-05-30 13:44:40 -0600

    edge for nest region added

--- Feature Extraction/getMazeEdgeRegions.m
+++ Feature Extraction/getMazeEdgeRegions.m
@@ -1,10 +1,10 @@
 function edgeStruct = getMazeEdgeRegions(quadrant, center_size)
 %
-% getMazeEdgeRegions: Return feeder and center zone edges for a given maze quadrant.
+% getMazeEdgeRegions: Return feeder, center and nest zone edges for a given maze quadrant.
 %
 %   edgeStruct = getMazeEdgeRegions(quadrant, center_size) returns a structure containing
-%   the x- and y-coordinate boundaries (edges) of the four feeder zones and the central zone 
-%   for a maze, based on the specified quadrant orientation.
+%   the x- and y-coordinate boundaries (edges) of the four feeder zones, central zone 
+%   and the nest for a maze, based on the specified quadrant orientation.
 %
 %   Input:
 %     - quadrant     : Integer (1 to 4), representing the spatial orientation of the maze.
@@ -17,6 +17,7 @@ function edgeStruct = getMazeEdgeRegions(quadrant, center_size)
 %         • Feeder3 : {x_range, y_range} for Feeder 3 (concentration 2)
 %         • Feeder4 : {x_range, y_range} for Feeder 4 (concentration 0.5)
 %         • Center  : {x_range, y_range} for the central zone
+%         • Nest    : {x_range, y_range} for the strip comprising 5% and 9%
 %
 %   Notes:
 %     - The coordinate edges are defined in normalized maze space.
@@ -48,6 +49,8 @@ switch quadrant
         x_edge_for_center = [(0.5 - center_size/2) (0.5 + center_size/2)];
         y_edge_for_center = [(0.5 - center_size/2) (0.5 + center_size/2)];
 
+        x_edge_for_nest = [-0.05 1.05]; y_edge_for_nest = [-0.05 0.25];
+
     case 2
         x_edge_for_9 = [-1.05 -0.75]; y_edge_for_9 = [0.75 1.05];
         x_edge_for_5 = [-1.05 -0.75]; y_edge_for_5 = [-0.05 0.25];
@@ -56,6 +59,8 @@ switch quadrant
 
         x_edge_for_center = [(-0.5 - center_size/2) (-0.5 + center_size/2)];
         y_edge_for_center = [(0.5 - center_size/2) (0.5 + center_size/2)];
+
+        x_edge_for_nest = [-1.05, -0.75]; y_edge_for_nest = [-0.05 1.05];
 
     case 3
         x_edge_for_9 = [-0.25 0.05]; y_edge_for_9 = [-1.05 -0.75];
@@ -66,6 +71,8 @@ switch quadrant
         x_edge_for_center = [(-0.5 - center_size/2) (-0.5 + center_size/2)];
         y_edge_for_center = [(-0.5 - center_size/2) (-0.5 + center_size/2)];
 
+        x_edge_for_nest = [-1.05 0.05]; y_edge_for_nest = [-1.05 -0.75];
+        
     case 4
         x_edge_for_9 = [-0.05 0.25]; y_edge_for_9 = [-0.25 0.05];
         x_edge_for_5 = [0.75 1.05]; y_edge_for_5 = [-0.25 0.05];
@@ -75,12 +82,15 @@ switch quadrant
         x_edge_for_center = [(0.5 - center_size/2) (0.5 + center_size/2)];
         y_edge_for_center = [(-0.5 - center_size/2) (-0.5 + center_size/2)];
 
+        x_edge_for_nest = [-0.05 1.05]; y_edge_for_nest = [-0.25 0.05];
+        
 end
 
 edgeStruct.Feeder1 = {x_edge_for_9, y_edge_for_9};
 edgeStruct.Feeder2 = {x_edge_for_5, y_edge_for_5};
 edgeStruct.Feeder3 = {x_edge_for_2, y_edge_for_2};
 edgeStruct.Feeder4 = {x_edge_for_0_5, y_edge_for_0_5};
-edgeStruct.Center = {x_edge_for_center, y_edge_for_center};
+edgeStruct.Center  = {x_edge_for_center, y_edge_for_center};
+edgeStruct.Nest    = {x_edge_for_nest, y_edge_for_nest};
 
 end
