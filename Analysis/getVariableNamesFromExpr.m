65a3b20717eb6339632ef3ce953006e1025bb604
atanu <atanurkm11@gmail.com>
2025-06-14 09:32:51 -0600

    Functions moved

--- Analysis/getVariableNamesFromExpr.m
+++
@@ -1,9 +0,0 @@
-function varNames = getVariableNamesFromExpr(expr)
-% Extract variable names from a mathematical expression string.
-
-% Remove numbers, parentheses, and operators
-tokens = regexp(expr, '[a-zA-Z_][a-zA-Z_0-9]*', 'match');
-% Remove MATLAB functions like 'mean', 'log' etc. if needed
-protectedWords = ["mean", "log", "exp", "min", "max", "sqrt"];
-varNames = setdiff(unique(tokens), protectedWords);
-end
